d4819da7d724450d55fba1c27085869ce9daed65
alxmart <alxmartinez@gmail.com>
2024-04-25 20:50:12 -0300

    Example using old method(findViewById)

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.luizafmartinez.m20_maiscomponentesinterface
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var btnClique: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +22,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Usando método antigo para fazer vinculação (findViewById)
+        btnClique = findViewById(R.id.btn_clique)
+
+        btnClique.setOnClickListener {
+            Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
