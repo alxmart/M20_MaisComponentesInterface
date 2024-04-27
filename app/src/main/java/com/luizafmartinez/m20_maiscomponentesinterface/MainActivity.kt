9eac71b153231005a559ebce9733129fd52f66db
alxmart <alxmartinez@gmail.com>
2024-04-27 09:33:06 -0300

    Implementing viewBinding.

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/MainActivity.kt
@@ -7,20 +7,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.m20_maiscomponentesinterface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     //private  lateinit var btnClique: Button
+    //private lateinit var binding: ActivityMainBinding
+    private val binding by lazy {
+        ActivityMainBinding.inflate( layoutInflater )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView( binding.root )
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        with(binding) {
+            btnClique?.setOnClickListener {  }
+            btnExecutar?.setOnClickListener {  } // apace no layout -> land
         }
 
         // Usando método antigo para fazer vinculação (findViewById)
@@ -30,6 +39,5 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
         }
         */
-
     }
 }
