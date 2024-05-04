8758c3af03711df55b172b3b3b1aa46681ebbfea
alxmart <alxmartinez@gmail.com>
2024-05-04 07:09:50 -0300

    Tests using AlertDialog.(method chaining version)

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/FormularioActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/FormularioActivity.kt
@@ -79,8 +79,38 @@ class FormularioActivity : AppCompatActivity() {
     }
 
     private fun caixaDialogoAlerta() {
+
+        AlertDialog.Builder(this)
+
+            .setTitle("Confirmar exclusão do item ?")
+            .setMessage("Tem certeza que quer remover ?")
+            .setNegativeButton("CANCELAR") { dialog, posicao ->
+                //dialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Cancelar clicado",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .setPositiveButton("REMOVER") { dialog, posicao ->
+                Toast.makeText(
+                    this,
+                    "Remover", Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setCancelable(false)
+            .setNeutralButton("Ajuda") { dialog, posicao ->
+                Toast.makeText(
+                    this,
+                    "Ajuda", Toast.LENGTH_SHORT
+                ).show()
+            }
+            .setIcon(R.drawable.ic_alerta_24)
+            .create()
+            .show()
+
         // Usa 2classes - Builder e AlertDialog
-        val alertBuilder = AlertDialog.Builder(this)
+        /*val alertBuilder = AlertDialog.Builder(this)
 
         alertBuilder.setTitle("Confirmar exclusão do item ?")
 
@@ -102,77 +132,88 @@ class FormularioActivity : AppCompatActivity() {
             ).show()
         }
 
-        val alertDialog = alertBuilder.create()
-        alertDialog.show()
+        alertBuilder.setCancelable(false)
 
+        alertBuilder.setNeutralButton("Ajuda") { dialog, posicao ->
+            Toast.makeText(
+                this,
+                "Ajuda", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        alertBuilder.setIcon(R.drawable.ic_alerta_24)
+
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()*/
 
     }
-        private fun exibirSnackBar(view: View) {
 
-            val snackBar = Snackbar.make(
+    private fun exibirSnackBar(view: View) {
+
+        val snackBar = Snackbar.make(
+            view,
+            "Alteração feita com Sucesso",
+            Snackbar.LENGTH_LONG
+        )
+
+        snackBar.setAction("Confirmar") {
+            Toast.makeText(this, "DESFAZER", Toast.LENGTH_LONG).show()
+        }
+
+        /*
+        snackBar.setTextColor(
+            //resources.getColor(R.color.purple_200) DEPRECATED
+            ContextCompat.getColor(this,R.color.deep_purple)
+            //android.R.color.holo_orange_dark
+        )
+
+        snackBar.setActionTextColor(
+            ContextCompat.getColor(
+            this,
+            R.color.deep_orange)
+        )
+
+        snackBar.setBackgroundTint(
+            ContextCompat.getColor(
+            this,
+            R.color.gray_green)
+        )
+        */
+
+        snackBar.show()
+    }
+
+    /*private fun exibirSnackBar(view: View) {
+
+            val snackBar = Snacbar.make(
                 view,
                 "Alteração feita com Sucesso",
                 Snackbar.LENGTH_LONG
-            )
+            ).show()
+        }*/
+    private fun switchToggle() {
+        val switchMarcado = binding.switchNotificacoes.isChecked
+        val toggleMarcado = binding.toggleAtivo.isChecked
 
-            snackBar.setAction("Confirmar") {
-                Toast.makeText(this, "DESFAZER", Toast.LENGTH_LONG).show()
-            }
-
-            /*
-            snackBar.setTextColor(
-                //resources.getColor(R.color.purple_200) DEPRECATED
-                ContextCompat.getColor(this,R.color.deep_purple)
-                //android.R.color.holo_orange_dark
-            )
-    
-            snackBar.setActionTextColor(
-                ContextCompat.getColor(
-                this,
-                R.color.deep_orange)
-            )
-    
-            snackBar.setBackgroundTint(
-                ContextCompat.getColor(
-                this,
-                R.color.gray_green)
-            )
-            */
-
-            snackBar.show()
-        }
-
-        /*private fun exibirSnackBar(view: View) {
-    
-                val snackBar = Snacbar.make(
-                    view,
-                    "Alteração feita com Sucesso",
-                    Snackbar.LENGTH_LONG
-                ).show()
-            }*/
-        private fun switchToggle() {
-            val switchMarcado = binding.switchNotificacoes.isChecked
-            val toggleMarcado = binding.toggleAtivo.isChecked
-
-            val texto = "Switch: $switchMarcado toggle: $toggleMarcado"
-            binding.textResultado.text = texto
-        }
-
-        private fun radioButton() {
-            val masculino = binding.rbMasculino.isChecked
-            //binding.textResultado.text = if (masculino) "Masculino" else "Feminino"
-
-            val idItemSelecionado = binding.rgSexo.checkedRadioButtonId
-            binding.textResultado.text = when (idItemSelecionado) {
-                R.id.rbMasculino -> "Masculino"
-                R.id.rbFeminino -> "Feminino"
-                else -> "Nada selecionado"
-            }
-
-            binding.rgSexo.clearCheck()
-        }
-
+        val texto = "Switch: $switchMarcado toggle: $toggleMarcado"
+        binding.textResultado.text = texto
     }
+
+    private fun radioButton() {
+        val masculino = binding.rbMasculino.isChecked
+        //binding.textResultado.text = if (masculino) "Masculino" else "Feminino"
+
+        val idItemSelecionado = binding.rgSexo.checkedRadioButtonId
+        binding.textResultado.text = when (idItemSelecionado) {
+            R.id.rbMasculino -> "Masculino"
+            R.id.rbFeminino -> "Feminino"
+            else -> "Nada selecionado"
+        }
+
+        binding.rgSexo.clearCheck()
+    }
+
+}
 /*
  private fun radioButton() {
      val selecionadoMasculino = binding.rbMasculino.isChecked
