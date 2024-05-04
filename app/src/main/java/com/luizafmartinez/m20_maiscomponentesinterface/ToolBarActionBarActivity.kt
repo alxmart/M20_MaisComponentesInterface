d0c6a3214570cf70fa22e5513ff9c0a17daedb49
alxmart <alxmartinez@gmail.com>
2024-05-04 15:39:14 -0300

    Configuring onOptionsItemSelected.

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
@@ -1,12 +1,16 @@
 package com.luizafmartinez.m20_maiscomponentesinterface
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ToolBarActionBarActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,8 +20,34 @@ class ToolBarActionBarActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         //supportActionBar?.hide()
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.item_adicionar -> {
+                Toast.makeText(this, "Adicionar", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_pesquisar -> {
+                Toast.makeText(this, "Pesquisar", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_configuracoes -> {
+                Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_sair -> {
+                Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+
 }
