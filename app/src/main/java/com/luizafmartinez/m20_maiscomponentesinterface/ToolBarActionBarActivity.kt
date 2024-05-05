c4742ba6bd77660ce061be2e22895453cbf53005
alxmart <alxmartinez@gmail.com>
2024-05-05 12:52:53 -0300

    Added images in drawable

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
@@ -7,31 +7,50 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.m20_maiscomponentesinterface.databinding.ActivityToolBarActionBarBinding
 
 class ToolBarActionBarActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityToolBarActionBarBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tool_bar_action_bar)
+
+        //setContentView(R.layout.activity_tool_bar_action_bar)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+    inicializarToolbar()
         //supportActionBar?.hide()
         //inicializarActionBar()
+    }
 
+    private fun inicializarToolbar() {
+
+        binding.tbPrincipal.title = "Youtube"
+        binding.tbPrincipal.setTitleTextColor(
+            ContextCompat.getColor(this, R.color.white)
+        )
+        //binding.tbPrincipal.subtitle = "Mais detalhes"
+        setSupportActionBar( binding.tbPrincipal )
     }
 
     private fun inicializarActionBar() {
 
         addMenuProvider(
-            object: MenuProvider {
+            object : MenuProvider {
                 override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                     menuInflater.inflate(R.menu.menu_principal, menu)
                 }
@@ -40,15 +59,18 @@ class ToolBarActionBarActivity : AppCompatActivity() {
 
                     when (menuItem.itemId) {
                         R.id.item_adicionar -> {
-                            Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_pesquisar -> {
-                            Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_configuracoes -> {
-                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_sair -> {
