af3a5a19d44dc9168ff3a76bb2206ac5c12c2ed9
alxmart <alxmartinez@gmail.com>
2024-05-05 19:16:55 -0300

    separating toolbar to reuse.

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
@@ -21,9 +21,6 @@ class ToolBarActionBarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        //setContentView(R.layout.activity_tool_bar_action_bar)
         setContentView(binding.root)
 
         inicializarToolbar()
@@ -34,12 +31,12 @@ class ToolBarActionBarActivity : AppCompatActivity() {
     private fun inicializarToolbar() {
 
         binding.includeToolbar.tbPrincipal.title = "Youtube"
-
         binding.includeToolbar.tbPrincipal.setTitleTextColor(
             ContextCompat.getColor(this, R.color.white)
         )
 
-        /*binding.tbPrincipal.inflateMenu(R.menu.menu_principal)
+        /*
+        binding.tbPrincipal.inflateMenu(R.menu.menu_principal)
 
         binding.tbPrincipal.setOnMenuItemClickListener { menuItem ->
 
@@ -76,15 +73,14 @@ class ToolBarActionBarActivity : AppCompatActivity() {
                     return@setOnMenuItemClickListener true
                     // true
                 }
-            }*/
+            }
+            */
 
+        //binding.tbPrincipal.subtitle = "Mais detalhes"
 
-    //binding.tbPrincipal.subtitle = "Mais detalhes"
-
-    setSupportActionBar( binding.includeToolbar.tbPrincipal ) //Se nao usar, vai inflar o menu
-    inicializarActionBar()
-
-}
+        setSupportActionBar(binding.includeToolbar.tbPrincipal) //Se nao usar, vai inflar o menu
+        inicializarActionBar()
+    }
 
     private fun inicializarActionBar() {
 
@@ -95,7 +91,6 @@ class ToolBarActionBarActivity : AppCompatActivity() {
                 }
 
                 override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-
                     when (menuItem.itemId) {
                         R.id.item_adicionar -> {
                             Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT)
@@ -108,26 +103,20 @@ class ToolBarActionBarActivity : AppCompatActivity() {
                         }
 
                         R.id.item_configuracoes -> {
-                            Toast.makeText(
-                                applicationContext,
-                                "Configurações",
-                                Toast.LENGTH_SHORT
-                            )
+                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT)
                                 .show()
                         }
 
                         R.id.item_sair -> {
-                            Toast.makeText(applicationContext, "Sair", Toast.LENGTH_SHORT)
-                                .show()
+                            Toast.makeText(applicationContext, "Sair", Toast.LENGTH_SHORT).show()
                         }
                     }
                     return true
                 }
-
             }
         )
     }
-
+}
     /*
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_principal, menu)
@@ -155,4 +144,3 @@ class ToolBarActionBarActivity : AppCompatActivity() {
         return true
     }
     */
-}
