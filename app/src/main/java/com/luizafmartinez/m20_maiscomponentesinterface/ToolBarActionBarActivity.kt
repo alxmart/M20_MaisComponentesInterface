1b4195987bd6be47c1f14186e23103dae2fbecf0
alxmart <alxmartinez@gmail.com>
2024-05-05 10:51:10 -0300

    ToolBar using addMenuProvider/implementing methods

--- app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
+++ app/src/main/java/com/luizafmartinez/m20_maiscomponentesinterface/ToolBarActionBarActivity.kt
@@ -2,10 +2,12 @@ package com.luizafmartinez.m20_maiscomponentesinterface
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -21,15 +23,50 @@ class ToolBarActionBarActivity : AppCompatActivity() {
             insets
         }
         //supportActionBar?.hide()
+        inicializarActionBar()
     }
 
+    private fun inicializarActionBar() {
+
+        addMenuProvider(
+            object: MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_principal, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+
+                    when (menuItem.itemId) {
+                        R.id.item_adicionar -> {
+                            Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT).show()
+                        }
+
+                        R.id.item_pesquisar -> {
+                            Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT).show()
+                        }
+
+                        R.id.item_configuracoes -> {
+                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT).show()
+                        }
+
+                        R.id.item_sair -> {
+                            Toast.makeText(applicationContext, "Sair", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    return true
+                }
+
+            }
+        )
+    }
+
+    /*
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_principal, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when (item.itemId) {
             R.id.item_adicionar -> {
                 Toast.makeText(this, "Adicionar", Toast.LENGTH_SHORT).show()
@@ -49,5 +86,5 @@ class ToolBarActionBarActivity : AppCompatActivity() {
         }
         return true
     }
-
+    */
 }
